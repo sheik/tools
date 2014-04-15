92ecb5e95a0f82355a8c2727e9161c13692b5951
Jeff Aigner <jeff@sudosu.me>
2014-04-15 00:47:29 +0000

    Adding compression / prog check

--- blocklist.sh
+++ blocklist.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 # Gets all public URLs from iblocklist and fetches them, combining them into one list
 
+PROG=`basename $0`
+
 if [ -z "$1" ]; then
-    echo "`basename $0`: No output file specified"
-    echo "usage: blocklist.sh [file]"
-    exit
+    echo "$PROG: No output file specified"
+    echo "usage: $PROG [file]"
+    exit 1
 fi
+
+# check for required commands 
+CMDS=( wget gzip chmod rm )
+
+for c in "${CMDS[@]}"; do 
+    command -v "$c" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        echo "$PROG: Unable to find $c. Please install it to continue."
+        exit 1
+    fi
+done
 
 OUTFILE="$1"
 
 # get a list of urls
 LISTS=$(wget -O - "https://www.iblocklist.com/lists.php" | egrep "list\.iblocklist\.com" | cut -d"'" -f12 | uniq)
 
+# clear output file
 echo "" > "$OUTFILE"
 
-# download and append to OUTFILE
+# download all lists and append to OUTFILE
 for i in $LISTS; do
     wget -O - "$i" | gunzip >> "$OUTFILE" 
 done
 
-rm "$OUTFILE.gz"
+rm -f $OUTFILE.gz > /dev/null 2>&1
+
 gzip $OUTFILE
+chmod 640 $OUTFILE.gz
+
+exit 0
